047aa7b329ae186e01c047e4043ea31cd61dc1ca
Funsoul <i@funs.cc>
2018-02-28 23:35:36 +0800

    增加文章置顶功能

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Funs.cc 
- * 
+ * Funs.cc
+ *
  * @package Funsoul
  * @author Funsoul
  * @version 1.0
@@ -10,13 +10,11 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  $this->need('header.php');
 ?>
-
 <?php
-
 /** 文章置顶 */
 $sticky = $sticky = $this->options->sticky; //置顶的文章cid，按照排序输入, 请以半角逗号或空格分隔
 if($sticky && $this->is('index') || $this->is('front')){
-    $sticky_cids = explode(',', strtr($sticky, ' ', ','));//分割文本 
+    $sticky_cids = explode(',', strtr($sticky, ' ', ','));//分割文本
     $sticky_html = "<span style='color:red'>[置顶] </span>"; //置顶标题的 html
     $db = Typecho_Db::get();
     $pageSize = $this->options->pageSize;
@@ -46,47 +44,41 @@ $uid = $this->user->uid; //登录时，显示用户各自的私密文章
 }
 
 ?>
-
-  <div class="blog container"> 
-   <div class="row"> 
-    <div class="blog-main col-sm-12 col-md-9"> 
-     <?php while($this->next()): ?>
-
-     <article id="blog-7110" class="post" itemscope="" itemtype="http://schema.org/BlogPosting"> 
-      <div class="blog-node media "> 
-       <h2 class="title yahei" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>" title="<?php $this->title() ?>" target="_blank"><?php $this->sticky(); $this->title(); ?></a></h2> 
-       <div class="blog-timg media-left">
+  <div class="blog container">
+    <div class="row">
+      <div class="blog-main col-sm-12 col-md-9">
+        <?php while($this->next()): ?>
+          <article id="blog-7110" class="post" itemscope="" itemtype="http://schema.org/BlogPosting">
+            <div class="blog-node media ">
+              <h2 class="title yahei" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>" title="<?php $this->title() ?>" target="_blank"><?php $this->sticky(); $this->title(); ?></a></h2>
+              <div class="blog-timg media-left">
 <?php if($this->options->slimg && 'guanbi'==$this->options->slimg): ?>
 <?php else: ?>
 <?php if($this->options->slimg && 'showoff'==$this->options->slimg): ?><a href="<?php $this->permalink() ?>" ><?php showThumbnail($this); ?></a>
 <?php else: ?>
 <a href="<?php $this->permalink() ?>" title="<?php $this->title() ?>" target="_blank"> <img src="<?php showThumbnail($this); ?>" /> </a>
 <?php endif; ?>
-        <?php endif; ?>
-       </div> 
-       <div class="media-body">
-         <?php $this->excerpt(250); ?> 
-        <div class="tags">
-         <?php $this->tags(' ', true); ?>
-         <a href="<?php $this->permalink() ?>" class="pull-right"><span class="glyphicon glyphicon-time" aria-hidden="true"></span> <time datetime="<?php $this->date('Y-m-d'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time></a>
-        </div> 
-       </div> 
-      </div> 
-     </article> 
-     <?php endwhile; ?>
-     <nav class="blog-nav"> 
-
+<?php endif; ?>
+              </div>
+              <div class="media-body">
+<?php $this->excerpt(250); ?>
+  <div class="tags">
+<?php $this->tags(' ', true); ?>
+  <a href="<?php $this->permalink() ?>" class="pull-right"><span class="glyphicon glyphicon-time" aria-hidden="true"></span> <time datetime="<?php $this->date('Y-m-d'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time></a>
+  </div>
+  </div>
+  </div>
+          </article>
+<?php endwhile; ?>
+  <nav class="blog-nav">
 <?php $this->pageLink('<x aria-label="Previous" class="btn btn-inverse btn-lage pull-left">上一页</x>','prev');?>
-
-      <a href="" class="btn btn-inverse" disabled="disabled">第 <?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?> 页 / 共 <?php echo   ceil($this->getTotal() / $this->parameter->pageSize); ?> 页</a> 
-
- <?php $this->pageLink('<x aria-label="Next" class="btn btn-inverse btn-lage pull-right">下一页</x>','next');?>
-
-     </nav> 
-    </div> 
-    <div class="blog-sidebar col-sm-12 col-md-3"> 
+  <a href="" class="btn btn-inverse" disabled="disabled">第 <?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?> 页 / 共 <?php echo   ceil($this->getTotal() / $this->parameter->pageSize); ?> 页</a>
+<?php $this->pageLink('<x aria-label="Next" class="btn btn-inverse btn-lage pull-right">下一页</x>','next');?>
+  </nav>
+  </div>
+  <div class="blog-sidebar col-sm-12 col-md-3">
 <?php $this->need('sidebar.php'); ?>
-    </div> 
-   </div> 
-  </div> 
-  <?php $this->need('footer.php'); ?>
+  </div>
+  </div>
+  </div>
+<?php $this->need('footer.php'); ?>
